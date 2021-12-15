a86feed388ccf387f1b336849e55af26b21fc384
Italo Gusmão <itamarques19@gmail.com>
2021-12-15 17:58:00 -0300

    atualizar api

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apis = {
-  development: "https://e-sport-iron.herokuapp.com",
+  development: "http://localhost:4000/",
   production: "https://e-sport-iron.herokuapp.com",
 };
 
