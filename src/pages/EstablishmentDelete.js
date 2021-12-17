c2da7a822a56e2af378b61266b12fe0df9f31237
Guilherme Santos Gomes <guilherme0500@hotmail.com>
2021-12-17 12:30:29 -0300

    link e rota de deletar

--- src/pages/EstablishmentDelete.js
+++ src/pages/EstablishmentDelete.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import api from "../../apis/api";
+import api from "../apis/api";
 
 function EstablishmentDelete() {
   const { id } = useParams();
